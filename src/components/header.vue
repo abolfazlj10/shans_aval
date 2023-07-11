c0ce6602b7b0962c5b38c0105f7c98493397d96e
abolfazl_j10 <abolfazljamshidi101010@gmail.com>
2023-07-11 18:03:10 +0330

    respansive header component

--- src/components/header.vue
+++ src/components/header.vue
@@ -23,7 +23,7 @@ export default{
                     if(this.word == this.words.length)
                     this.word = 0
                 }, 1500);
-            }, 5000);
+            }, 4000);
         }
     },
     created(){
@@ -34,12 +34,12 @@ export default{
 <template>
     <div class="getStarted">
         <div class="ContainerHeader">
-            <div class="flex flex-col h-full justify-around w-6/12">
-                <div class="space-y-10 text-right">
-                    <div class="text-2xl flex gap-1"><div ref="text" class="text">{{ titr}}{{ words[word] }}</div><div class="typer"></div><div>قرعه کشی</div></div>
-                    <div class="text-[#FFF]/80 text-sm">با پلتفرم شانس اول این امکان فراهم شده است که قرعه کشی های خود را ایجاد کنید و شرکت کننده های خود را مدیریت کنید یا شرایط قرعه کشی را جهت ثبت نام مورد نظر بگیرید.</div>
+            <div class="flex flex-col h-full justify-around w-6/12 max-md:ml-auto max-md:w-8/12 max-md:gap-7 max-sm:w-9/12 max-sm:gap-12 max-[539px]:w-full">
+                <div class="space-y-10 text-right max-[500px]:text-center">
+                    <div class="text-2xl max-lg:text-lg flex gap-1  max-[500px]:justify-center max-[416px]:text-[17px] max-[395px]:text-[16px] max-[376px]:text-[15px]"><div ref="text" class="text">{{ titr}}{{ words[word] }}</div><div class="typer"></div><div>قرعه کشی</div></div>
+                    <div class="text-[#FFF]/80 text-sm max-sm:text-[13px] max-[376px]:text-[12px]">با پلتفرم شانس اول این امکان فراهم شده است که قرعه کشی های خود را ایجاد کنید و شرکت کننده های خود را مدیریت کنید یا شرایط قرعه کشی را جهت ثبت نام مورد نظر بگیرید.</div>
                 </div>
-                <div class="flex justify-end gap-3">
+                <div class="flex justify-end gap-3 max-[500px]:justify-center">
                     <button class="btnGetStarted group">
                         <div title="راهنمای شانس اول">راهنما</div>
                         <i class="ri-file-list-2-fill icon"></i>
@@ -51,22 +51,22 @@ export default{
                     
                 </div>
             </div>
-            <img class="w-[600px]" src="../assets/img/1.png" alt="vector">
+            <img class="w-[600px] max-xl:w-[500px] max-lg:w-[360px] max-md:mr-auto max-md:w-[300px] max-[539px]:ml-auto" src="../assets/img/1.png" alt="vector">
         </div>
     </div>
 </template>
 <style scoped>
 .getStarted{
-    @apply bg-brand text-center w-full h-[500px];
+    @apply bg-brand text-center w-full h-[500px] max-lg:h-[450px] max-md:h-fit max-md:p-5;
 }
 .ContainerHeader{
-    @apply h-full container flex items-center justify-between text-white;
+    @apply h-full container flex items-center justify-between text-white max-xl:gap-5 max-md:flex-col max-md:gap-9 max-sm:gap-14;
 }
 .btnGetStarted{
-    @apply flex flex-row-reverse gap-2 border px-5 py-1 rounded-2xl duration-100 hover:-translate-y-1 shadow-lg hover:shadow-xl;
+    @apply flex flex-row-reverse gap-2 border px-5 py-1 rounded-2xl duration-100 hover:-translate-y-1 shadow-lg hover:shadow-xl max-sm:text-sm;
 }
 .btnAdd{
-    @apply flex flex-row-reverse gap-2  border px-5 py-1 rounded-2xl bg-white text-brand duration-100 hover:-translate-y-1 shadow-lg hover:shadow-xl;
+    @apply flex flex-row-reverse gap-2  border px-5 py-1 rounded-2xl bg-white text-brand duration-100 hover:-translate-y-1 shadow-lg hover:shadow-xl max-sm:text-sm;
 }
 .icon{
     @apply duration-75 group-hover:animate-spin;
