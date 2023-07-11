5e7463e2cd10427d175a102887c5c65bc65717bd
abolfazl_j10 <abolfazljamshidi101010@gmail.com>
2023-07-11 18:02:40 +0330

    update respansive component nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="nav">
         <div class="title"><router-link to="/">شانس اول</router-link></div>
-        <div class="linkNav text-sm">
+        <div class="linkNav">
             <div class="linkItem group">
                 <div class="borderbottom"></div>
                 <router-link to="/">ایجاد</router-link>
@@ -21,22 +21,22 @@
         </div>
         <router-link to="/" class="login group">
             <div>ورود</div>
-            <i class="ri-login-box-line text-xl duration-75 group-hover:scale-105"></i>
+            <i class="ri-login-box-line text-xl duration-75 group-hover:scale-105 max-sm:text-[18px]"></i>
         </router-link>
     </div>
 </template>
 <style scoped>
 .nav{
-    @apply container flex items-center gap-7 py-2 border-b max-md:flex-wrap  ;
+    @apply container flex items-center gap-7 py-2 border-b max-md:flex-wrap max-sm:px-7 ;
 }
 .title{
-    @apply text-2xl text-brand max-md:order-1 max-md:border-b max-md:border-brand max-md:py-2;
+    @apply text-2xl text-brand max-md:order-1 max-md:border-b max-md:border-brand max-md:py-2 max-sm:text-xl;
 }
 .linkNav{
-    @apply flex gap-12 max-md:w-full max-md:order-3 max-md:justify-center max-[400px]:gap-7 max-[332px]:gap-3;
+    @apply flex gap-12 max-md:w-full max-md:order-3 max-md:justify-center max-[400px]:gap-7 max-[332px]:gap-3 text-sm max-sm:text-xs;
 }
 .login{
-    @apply flex flex-row-reverse items-center gap-4 mr-auto text-center bg-brand rounded-md text-white py-2 px-8 max-md:order-2;
+    @apply flex flex-row-reverse items-center gap-4 mr-auto text-center bg-brand rounded-md text-white py-2 px-8 max-md:order-2 max-sm:text-xs max-sm:px-6 max-sm:py-1;
 }
 .linkItem{
     @apply flex flex-col-reverse gap-[1px];
