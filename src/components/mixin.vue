ff781e48d1d4abe44b24dd97518fbc548d2cb4c5
abolfazl_j10 <abolfazljamshidi101010@gmail.com>
2023-07-16 14:50:00 +0330

    create file mixin for variable global

---
+++ src/components/mixin.vue
@@ -0,0 +1,9 @@
+<script>
+export default{
+    data(){
+        return{
+            url:'http://localhost:3000'
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/style/base.css'
 import 'remixicon/fonts/remixicon.css'
+import mixin from './components/mixin.vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +9,6 @@ import router from './router.js'
 const app = createApp(App)
 
 app.use(router)
+app.mixin(mixin)
 
 app.mount('#app')
