b6f1e7ca0129c6b6a2040b554033865a65a1a4cc
abolfazl_j10 <abolfazljamshidi101010@gmail.com>
2023-07-17 16:45:24 +0330

    add request fetch for forgot password

--- src/pages/auth/forgot.vue
+++ src/pages/auth/forgot.vue
@@ -7,7 +7,10 @@ export default{
             prentWidth:0,
             widthForm:0,
             isShow:false,
-            step:0
+            step:0,
+            email:'abolfazljamshidi101010@gmail.com',
+            code:null,
+            password:null
         }
     },
     methods:{
@@ -23,6 +26,50 @@ export default{
                 let width = this.removePX(getComputedStyle(inp).width)
                 this.widthForm = width - padding
             }catch{}
+        },
+        async forgot(type){
+            const data = {
+                type:type,
+                email:this.email,
+                code:this.code,
+                pass:this.password
+            }
+
+            const url = this.url + '/forgotPass'
+            const req = await fetch(url,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                    'Accept':'application/json'
+                },
+                body:JSON.stringify(data)
+            })
+            const res = await req.json()
+            if(!res.succes)
+                alert('خطا')
+            else if(res.status == 'existsEmail')
+                alert('کاربری با این ایمیل وجود ندارد لطفا ایمیل معتبری وارد کنید.')
+            else if(res.status == 'sendCode'){
+                this.step = 1
+                setTimeout(() => {
+                    this.$refs.code.focus()
+                }, 200);
+            }else if(res.status == 'validCode'){
+                this.step = 2
+                setTimeout(() => {
+                    this.$refs.pass.focus()
+                }, 200);
+            }else if(res.status == 'invalidCode'){
+                alert('کد وارد شده اشتباه است.')
+                this.code = null
+            }else if(res.status == 'succes'){
+                alert('رمز با موفقیت تغییر کرد.')
+                this.step = 0
+                this.email = null
+                this.password = null
+                this.code = null
+                this.$router.push('/')
+            }            
         }
     },
     mounted(){
@@ -39,24 +86,24 @@ export default{
         <div class="flex items-center flex-grow text-2xl">بازیابی رمز عبور</div>
         <div :style="`width:${widthForm}px`" class="flex-grow overflow-hidden">
             <div :style="`width:${widthForm*3}px;transform: translate3d(${widthForm * step}px, 0, 0);`" class="justify-center flex duration-150 overflow-hidden">
-                <form @submit.prevent="step = 1" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
+                <form @submit.prevent="forgot('email')" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
                     <div class="inputs group">
                         <i class="ri-mail-line iconInput"></i>
-                        <input required @focus="propsParent.changer(1)" ref="email" type="email" placeholder="ایمیل" class="inputStyle">
+                        <input required @focus="propsParent.changer(1)" v-model="email" ref="email" type="email" placeholder="ایمیل" class="inputStyle">
                     </div>
                     <button class="loginBtn">ارسال</button>
                 </form>
-                <form @submit.prevent="step = 2" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
+                <form @submit.prevent="forgot('code')" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
                     <div class="inputs group">
-                        <i class="ri-mail-line iconInput"></i>
-                        <input required @focus="propsParent.changer(6)" ref="code" type="text" minlength="6" placeholder="کد 6 رقمی" class="inputStyle">
+                        <i class="ri-mail-send-line iconInput"></i>
+                        <input required v-model="code" @focus="propsParent.changer(6)" ref="code" type="text" minlength="6" placeholder="کد 6 رقمی ارسال شده" class="inputStyle">
                     </div>
                     <button class="loginBtn">تایید</button>
                 </form>
-                <form @submit.prevent="step = 0" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
+                <form @submit.prevent="forgot('password')" :style="`width:${widthForm}px`" class="flex flex-col gap-5">
                     <div class="inputs group">
                         <i class="ri-lock-2-line iconInput"></i>
-                        <input required @focus="propsParent.changer(2)" ref="pass"  :type="seePass ? 'password' : 'text'" minlength="8" placeholder="رمز جدید" class="inputStyle">
+                        <input required @focus="propsParent.changer(2)" v-model="password" ref="pass"  :type="seePass ? 'password' : 'text'" minlength="8" placeholder="رمز جدید" class="inputStyle">
                         <i v-if="!seePass" @click="seePass = true;focusPass()" class="ri-eye-line cursor-pointer iconInput"></i>
                         <i v-else @click="seePass = false;focusPass()" class="ri-eye-off-line cursor-pointer iconInput"></i>
                     </div>
