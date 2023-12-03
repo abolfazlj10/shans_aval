c1b161c73ab881646cb372fbeabc2a697258776e
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2023-12-03 15:27:05 +0330

    create cadr top lotterys

--- src/pages/Lotterys.vue
+++ src/pages/Lotterys.vue
@@ -7,8 +7,55 @@ export default{
 </script>
 <template>
     <Nav/>
-
+    <div class="container mt-10">
+        <div class="boxDesc">
+            <div class="contentDesc">
+                <div class="text-2xl">قرعه کشی ها</div>
+                <div>لیست تمامی قرعه کشی های پلفترم شانس اول</div>
+                <div>
+                    <ul class="recumendedLottery">
+                        <li>1 ) خانگی</li>
+                        <li>2 ) یخچال</li>
+                        <li>3 ) ماشین</li>
+                    </ul>
+                </div>
+                <router-link to="/New" class="btnAdd group">
+                    <div title="اگر ثبت نام نکرده اید ثبت نام کنید.">ایجاد</div>
+                    <i class="ri-add-box-fill icon"></i>
+                </router-link>
+            </div>
+            <img class="imageDesc" src="../../public/wheel of luck.jpg">
+        </div>
+    </div>
 </template>
 <style scoped>
-
+.boxDesc{
+    @apply bg-brand h-[580px] rounded-lg flex text-[#fff] px-5 py-7;
+}
+.contentDesc{
+    @apply flex-grow flex flex-col gap-10 text-center px-8;
+}
+.imageDesc{
+    @apply w-9/12 object-cover rounded-lg;
+}
+.recumendedLottery{
+    @apply text-right space-y-3;
+}
+.btnAdd{
+    @apply flex flex-row-reverse text-lg gap-2 mt-auto justify-center border p-2 mx-2 rounded-xl bg-white text-brand duration-100 hover:mx-0 hover:scale-105 shadow-lg hover:shadow-xl max-sm:text-sm;
+}
+.icon{
+    @apply duration-75 group-hover:animate-spin;
+}
+@keyframes spin {
+    0%{
+        transform: rotate(-10deg);
+    }
+    50%{
+        transform: rotate(10deg);
+    }
+    100%{
+        transform: rotate(-10deg);
+    }
+}
 </style>
